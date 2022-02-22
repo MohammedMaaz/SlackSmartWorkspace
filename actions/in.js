bedd5041236754fe8cc5f1f504c727c46e69f314
HamzaKhan9 <hamzalodhi03163045212@gmail.com>
2022-02-22 22:56:37 +0500

    temp

--- actions/in.js
+++ actions/in.js
@@ -1,12 +1,14 @@
+import firebase from "../utils/firebase";
+
 export default async ({ message, say }) => {
   await say(`Hey <@${message.user}>! Your time has been recorded! `);
-  // await firebase
-  //   .firestore()
-  //   .collection("logs")
-  //   .add({
-  //     timestamp: new Date(),
-  //     message: message.text,
-  //     user: message.user,
-  //     username: message.username || "",
-  //   });
+  await firebase
+    .firestore()
+    .collection("logs")
+    .add({
+      timestamp: new Date(),
+      message: message.text,
+      user: message.user,
+      username: message.username || "",
+    });
 };
