a47635ca03d17f8628425ce166a47d2f8d08a223
HamzaKhan9 <hamzalodhi03163045212@gmail.com>
2022-02-24 21:49:30 +0500

    attendance

--- actions/in.js
+++ actions/in.js
@@ -8,17 +8,17 @@ export default async ({ message, say }) => {
   //   .collection("attendance")
   //   .doc(date.toDateString());
   let perm = await refs.attendance.doc(date.toDateString()).get({ perm });
-  if (perm === "yes") {
-    await say(`Hey <@${message.user}>! Your time has been recorded!! `);
-    await firebase
-      .firestore()
-      .collection("attendance")
-      .doc(date.toDateString())
-      .set({
-        timestamp: new Date(),
-        status: message.text,
-        user: message.user,
-        username: message.username || "",
-      });
-  }
+  // if (perm === "yes") {
+  await say(`Hey <@${message.user}>! Your time has been recorded!! `);
+  await firebase
+    .firestore()
+    .collection("attendance")
+    .doc(date.toDateString())
+    .set({
+      timestamp: new Date(),
+      status: message.text,
+      user: message.user,
+      username: message.username || "",
+    });
+  // }
 };
