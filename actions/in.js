e3f920f166a6f0110a5d10cff55356c8886af971
HamzaKhan9 <hamzalodhi03163045212@gmail.com>
2022-02-24 21:25:04 +0500

    attendance

--- actions/in.js
+++ actions/in.js
@@ -4,16 +4,25 @@ import State from "./index";
 export default async ({ message, say }) => {
   // if (State.state) {
   let date = new Date();
-  let todayAttendance = firebase.firestore
+  // let todayAttendance = firebase.firestore
+  //   .collection("attendance")
+  //   .doc(date.toDateString());
+  let status = await firebase
+    .firestore()
     .collection("attendance")
-    .doc(date.toDateString());
-  // if (todayAttendance.get({ status }) !== "in") {
-  await say(`Hey <@${message.user}>! Your time has been recorded!! `);
-  await todayAttendance.set({
-    timestamp: new Date(),
-    status: message.text,
-    user: message.user,
-    username: message.username || "",
-  });
-  // }
+    .doc(date.toDateString())
+    .get({ status });
+  if (status !== "in") {
+    await say(`Hey <@${message.user}>! Your time has been recorded!! `);
+    await firebase
+      .firestore()
+      .collection("attendance")
+      .doc(date.toDateString())
+      .set({
+        timestamp: new Date(),
+        status: message.text,
+        user: message.user,
+        username: message.username || "",
+      });
+  }
 };
