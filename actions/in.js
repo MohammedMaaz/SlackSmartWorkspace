51fabd82894b9ec49aa4fb1f88845d9d7d7bb7b8
HamzaKhan9 <hamzalodhi03163045212@gmail.com>
2022-02-24 21:54:53 +0500

    attendance

--- actions/in.js
+++ actions/in.js
@@ -3,22 +3,18 @@ import State from "./index";
 
 export default async ({ message, say }) => {
   // if (State.state) {
-  let date = new Date();
+  let date = new Date().toDateString();
   // let todayAttendance = firebase.firestore
   //   .collection("attendance")
   //   .doc(date.toDateString());
   // let perm = await refs.attendance.doc(date.toDateString()).get({ perm });
   // if (perm === "yes") {
   await say(`Hey <@${message.user}>! Your time has been recorded!! `);
-  await firebase
-    .firestore()
-    .collection("attendance")
-    .doc(date.toDateString())
-    .set({
-      timestamp: new Date(),
-      status: message.text,
-      user: message.user,
-      username: message.username || "",
-    });
+  await refs.attendance.doc(date).set({
+    timestamp: new Date(),
+    status: message.text,
+    user: message.user,
+    username: message.username || "",
+  });
   // }
 };
