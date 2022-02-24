518fa85f3fd8c5de4a3174f7d1c7bf250d121181
HamzaKhan9 <hamzalodhi03163045212@gmail.com>
2022-02-24 16:25:50 +0500

    state mantain at in.js

--- actions/in.js
+++ actions/in.js
@@ -1,14 +1,18 @@
 import firebase from "../utils/firebase";
+export default state=true;
 
 export default async ({ message, say }) => {
-  await say(`Hey <@${message.user}>! Your time has been recorded!! `);
-  await firebase
-    .firestore()
-    .collection("logs")
-    .add({
-      timestamp: new Date(),
-      message: message.text,
-      user: message.user,
-      username: message.username || "",
-    });
-};
+  if(state){
+    await say(`Hey <@${message.user}>! Your time has been recorded!! `);
+    await firebase
+      .firestore()
+      .collection("logs")
+      .add({
+        timestamp: new Date(),
+        message: message.text,
+        user: message.user,
+        username: message.username || "",
+      });
+      state = false;
+  };
+}
