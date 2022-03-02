3259903136f6eb77df568b0bc6dc3ef2ab25c38e
HamzaKhan9 <hamzalodhi03163045212@gmail.com>
2022-03-02 13:32:08 +0500

    update

--- actions/in.js
+++ actions/in.js
@@ -4,14 +4,29 @@ export default async ({ message, say }) => {
   let date = new Date();
   let dateString = date.toDateString();
   let timeString = date.toTimeString();
-  // let state = await refs.attendance.doc(dateString).get(state);
-  // if (state === "out" || state === undefined || state === "") {
-  await say(`Hey <@${message.user}>! You are IN at ${timeString}.`);
-  await refs.attendance.doc(dateString).set({
-    timestampOfIn: new Date(),
-    state: message.text,
-    user: message.user,
-    username: message.username || "",
-  });
+  // function getInfo(data){
+  //   data.forEach(doc => {
+  //     let info =
+  //   });
   // }
+  let obj;
+  const snapshot = await refs.attendance.get();
+  await refs.attendance.get().then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      obj = doc.data();
+    });
+  });
+  let condition = obj.user;
+  // status = status.state;
+  if (condition === "out" || condition === undefined || condition === "") {
+    await say(`Hey <@${message.user}>! You are IN at ${timeString}.`);
+    await refs.attendance.doc(dateString).set({
+      timestampOfIn: new Date(),
+      state: message.text,
+      user: message.user,
+      // done: new Date(),
+      username: message.username || "",
+      // cond: condition,
+    });
+  }
 };
