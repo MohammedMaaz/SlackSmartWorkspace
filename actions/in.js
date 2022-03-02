c8f93fcee9166b5bc67d2066935d32100dcabfad
Mohammedanas7456 <mohammedanas7456@gmail.com>
2022-03-02 15:01:45 +0500

    out

--- actions/in.js
+++ actions/in.js
@@ -9,24 +9,24 @@ export default async ({ message, say }) => {
   //     let info =
   //   });
   // }
-  let obj;
-  const snapshot = await refs.attendance.get();
-  await refs.attendance.get().then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      obj = doc.data();
-    });
-  });
-  let condition = obj.user;
+  // let obj;
+  // const snapshot = await refs.attendance.get();
+  // await refs.attendance.get().then((snapshot) => {
+  //   snapshot.docs.forEach((doc) => {
+  //     obj = doc.data();
+  //   });
+  // });
+  // let condition = obj.user;
   // status = status.state;
-  if (condition === "out" || condition === undefined || condition === "") {
-    await say(`Hey <@${message.user}>! You are IN at ${timeString}.`);
-    await refs.attendance.doc(dateString).set({
-      timestampOfIn: new Date(),
-      state: message.text,
-      user: message.user,
-      // done: new Date(),
-      username: message.username || "",
-      // cond: condition,
-    });
-  }
+  // if (condition === "out" || condition === undefined || condition === "") {
+  await say(`Hey <@${message.user}>! You are IN at ${timeString}.`);
+  await refs.attendance.doc(dateString).set({
+    timestampOfIn: new Date(),
+    state: message.text,
+    user: message.user,
+    // done: new Date(),
+    username: message.username || "",
+    // cond: condition,
+  });
+  // }
 };
--- actions/out.js
+++ actions/out.js
@@ -4,11 +4,15 @@ export default async ({ message, say }) => {
   let date = new Date();
   let dateString = date.toDateString();
   let timeString = date.toTimeString();
-  await say(`Hey <@${message.user}>! You are OUT at ${timeString}.`);
-  await refs.attendance.doc(dateString).update({
-    timestampOfOut: new Date(),
-    state: message.text,
-    user: message.user,
-    username: message.username || "",
-  });
+  let docref = await refs.attendance.doc(dateString).get();
+  let state = docref.data().state;
+  if (state === "in") {
+    await say(`Hey <@${message.user}>! You are OUT at ${timeString}.`);
+    await refs.attendance.doc(dateString).update({
+      timestampOfOut: new Date(),
+      state: message.text,
+      user: message.user,
+      username: message.username || "",
+    });
+  }
 };
